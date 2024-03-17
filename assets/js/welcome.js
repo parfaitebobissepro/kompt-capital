1502d710e44283ee888c2af678e649c2d4493a6c
parfaitebobissepro <parfaitebobissepro@gmail.com>
2024-03-17 18:23:05 +0100

    restructuration

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -3,7 +3,21 @@
 
 var isReaded = false;
 
-console.log(localStorage.getItem('isFirstTimeOpened'));
+let contentScrollable = document.querySelector('#welcomeModal .welocme-content');
+
+contentScrollable.addEventListener('scroll', function() {
+    // Calcul de la hauteur totale du contenu à l'intérieur de l'élément scrollable
+    var scrollableContentHeight = contentScrollable.scrollHeight;
+    // Calcul de la position actuelle du défilement
+    var currentScrollPosition = contentScrollable.scrollTop + contentScrollable.clientHeight;
+
+    // Vérification si l'utilisateur a fait défiler jusqu'en bas
+    if (currentScrollPosition >= scrollableContentHeight) {
+        isReaded = true;
+        console.log("Utilisateur a fait défiler jusqu'en bas.");
+        // Vous pouvez également effectuer d'autres actions ici en cas de défilement jusqu'en bas
+    }
+});
 
 if (localStorage.getItem('isFirstTimeOpened') == null) {
     localStorage.setItem('isFirstTimeOpened', 1);
@@ -46,5 +60,5 @@ function selectRole(element) {
 
     element.classList.add('active');
 
-    document.querySelector(".acceptButtonWelcome").disabled = false;
+    document.querySelector(".acceptButtonWelcome").disabled = false && isReaded;
 }
